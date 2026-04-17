a39e8f355349839d7d1646b6decec36d688a161c
J-Miguel Rocha Ramos <j-miguelrocharamos@MacBookPro.fritz.box>
2026-04-17 14:06:01 +0200

    add the missing docstrings

--- snowman.py
+++ snowman.py
@@ -1,6 +1,15 @@
 from game_logic import play_game
 
 def main():
+    """
+    Run the main game loop.
+
+    Starts the game once, then repeatedly asks the user if they want
+    to play again. If the user enters 'y', the game restarts. If the
+    user enters 'n', the program exits. Any other input prompts the
+    user to try again.
+    """
+
     play_game()
     while True:
         play_again = input(
